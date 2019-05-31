f892053dd4c78109139c824d76346cbe631c848f
jineyne <jineyne@gmail.com>
2019-05-31 23:50:09 +0900

    init glfw & window

--- main.cpp
+++ main.cpp
@@ -1,21 +1,52 @@
 #include <cstdlib>
+#include <cstdint>
+#include <cassert>
 #include <iostream>
 
 #include <vulkan/vulkan.h>
 
+#define GLFW_INCLUDE_VULKAN
+
+#include <GLFW/glfw3.h>
+
 class VulkanApplication {
+private:
+    const uint32_t Width = 800, Height = 600;
+
+    GLFWwindow *mWindow = nullptr;
+
 public:
     void run() {
         // init
+        initWindow();
         initVulkan();
 
         mainLoop();
 
         // deinit
         deinitVulkan();
+        deinitWindow();
     }
 
 private:
+    void initWindow() {
+        // init glfw library
+        if (!glfwInit()) {
+            throw std::runtime_error("failed to init glfw");
+        }
+
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+        mWindow = glfwCreateWindow(Width, Height, "Vulkan Application", nullptr, nullptr);
+        assert(mWindow != nullptr);
+    }
+
+    void deinitWindow() {
+        glfwDestroyWindow(mWindow);
+        glfwTerminate();
+    }
+
     void initVulkan() {
 
     }
@@ -25,7 +56,9 @@ private:
     }
 
     void mainLoop() {
-
+        while (!glfwWindowShouldClose(mWindow)) {
+            glfwPollEvents();
+        }
     }
 };
 
