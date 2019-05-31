d43e1023569585e3e918056529249d7aea7e6f6a
jineyne <jineyne@gmail.com>
2019-05-31 23:31:24 +0900

    init base code

--- main.cpp
+++ main.cpp
@@ -1,7 +1,42 @@
 #include <cstdlib>
 #include <iostream>
 
+#include <vulkan/vulkan.h>
+
+class VulkanApplication {
+public:
+    void run() {
+        // init
+        initVulkan();
+
+        mainLoop();
+
+        // deinit
+        deinitVulkan();
+    }
+
+private:
+    void initVulkan() {
+
+    }
+
+    void deinitVulkan() {
+
+    }
+
+    void mainLoop() {
+
+    }
+};
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Running!" << std::endl;
+    VulkanApplication app;
+    try {
+        app.run();
+    } catch (std::exception &e) {
+        std::cerr << "Failed: " << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
     return EXIT_SUCCESS;
 }
