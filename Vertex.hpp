2300a9354d05d54b72d19155ec35ef94cc59b1e4
jineyne <jineyne@gmail.com>
2019-06-02 14:59:36 +0900

    add vertex

---
+++ Vertex.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <vulkan/vulkan.h>
+#include <glm/glm.hpp>
+#include <vector>
+#include <array>
+
+struct Vertex {
+    glm::vec2 pos;
+    glm::vec3 color;
+
+    static VkVertexInputBindingDescription getBindingDescription();
+
+    static std::array<VkVertexInputAttributeDescription, 2> getAttributeDescriptions();
+};
